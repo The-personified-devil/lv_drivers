23375b6459c350e3a302c28084518d6c7993830a
The personified devil <leonhard60-git@yahoo.com>
2021-02-24 11:32:51 +0100

    Made some modifications to allow rebuilding the folder structure in the compile directory

--- lv_drivers.mk
+++ lv_drivers.mk
@@ -1,7 +1,6 @@
 LV_DRIVERS_DIR_NAME ?= lv_drivers
 
-CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/*.c)
-CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/indev/*.c)
-CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/gtkdrv/*.c)
-CSRCS += $(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/display/*.c)
-
+CSRCS += $(subst  $(LVGL_DIR)/,,$(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/*.c))
+CSRCS += $(subst  $(LVGL_DIR)/,,$(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/indev/*.c))
+CSRCS += $(subst  $(LVGL_DIR)/,,$(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/gtkdrv/*.c))
+CSRCS += $(subst  $(LVGL_DIR)/,,$(wildcard $(LVGL_DIR)/$(LV_DRIVERS_DIR_NAME)/display/*.c))
